1631b52da56aa36fd0cfbae30ceef324ed21279d
VVicheka <150057081+VVicheka@users.noreply.github.com>
2025-06-13 17:19:10 +0700

    config railway

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000',
+                        'drink-order-client.vercel.app'
+    ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '/^https:\/\/.*\.vercel\.app$/'
+    ],
 
     'allowed_headers' => ['*'],
 
